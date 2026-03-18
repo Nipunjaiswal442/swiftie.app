869fe7c82c323912630a1f82d1d3e98086a8c143
Nipun Jaiswal <nipun.24bce7867@vitapstudent.ac.in>
2026-03-19 03:26:55 +0530

    Initial Commit

---
+++ studloopTests/studloopTests.swift
@@ -0,0 +1,16 @@
+//
+//  studloopTests.swift
+//  studloopTests
+//
+//  Created by Apple on 19/03/26.
+//
+
+import Testing
+
+struct studloopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
